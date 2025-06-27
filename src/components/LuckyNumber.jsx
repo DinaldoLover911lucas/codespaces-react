38f4474123e3e9ff39f750db51331f3d4b6e967a
Lucas antonio <melo.coutinho@escolar.ifrn.edu.br>
2025-06-27 23:10:29 +0000

    desafio4

--- src/components/LuckyNumber.jsx
+++ src/components/LuckyNumber.jsx
@@ -1,34 +1,36 @@
-import styles from './LuckyNumber.Module.css';
-import { useState } from 'react';
+import { useState } from "react";
+import styles from "./LuckyNumber.module.css";
 
 export function LuckyNumber() {
-    const [luckyNumber, setLuckyNumber] = useState(0); 
-    var [array, setArray] = useState(["0, "]);
-    var [msg, setMsg] = useState("");
-    function handleClick() {
-        var result = Math.floor(Math.random() * 40) + 1;
-        setLuckyNumber(result);
-        if (array.includes((result + ", "))) {
-            setMsg("Este número já foi sorteado!");
-        } else {
-            setMsg("");
-            setArray(0 + ", ");
-            array.push(result+ ", ");
-            setArray(array);
-        }
-        
-    }
-    
-    return <div className ={styles.container}>
+  const [luckyNumber, setLuckyNumber] = useState(0);
+  const [array, setArray] = useState([]);
+  const [message, setMessage] = useState("");
 
-            {luckyNumber === 0 ? (
-                <h1>Lucky Number 🎲</h1>) : (
-                <h1> Lucky Number: = {luckyNumber}</h1>)}
-            
-            <button className={styles.button} onClick={handleClick}>
-                I'm Feeling Lucky Today!
-            </button>
-            <h1>{array}</h1>
-            <h1>{msg}</h1>
-        </div>
+  function handleClick() {
+    var n = Math.floor(Math.random() * 40) + 1;
+    setLuckyNumber(n);
+    
+    if (array.includes(n)) {
+      setMessage(`The number ${n} has already been chosen!`);
+    } else {
+      setMessage("");
+      setArray([...array, n]);
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      {luckyNumber === 0 ? (
+        <h1>Lucky Number 🎲</h1>
+      ) : (
+        <h1>Lucky Number = {luckyNumber}</h1>
+      )}
+      {message ? <p>{message}</p> : null}
+      <button className={styles.button} onClick={handleClick}>
+        I'm Feeling Lucky Today!
+      </button>
+      <h3>Lucky Numbers Array:</h3>
+      <p>[{[...array].toString()}]</p>
+    </div>
+  );
 }
