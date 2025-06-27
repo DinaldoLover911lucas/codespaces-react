709d4b0d86968625dcdb0109d0983a34ae0ad72b
Lucas antonio <melo.coutinho@escolar.ifrn.edu.br>
2025-06-27 02:50:14 +0000

    luckynumber

--- src/components/LuckyNumber.jsx
+++ src/components/LuckyNumber.jsx
@@ -1,20 +1,34 @@
-import { useState } from 'react';
 import styles from './LuckyNumber.Module.css';
+import { useState } from 'react';
 
 export function LuckyNumber() {
-const [LuckyNumber, setLuckyNumber] = useState(0);
+    const [luckyNumber, setLuckyNumber] = useState(0); 
+    var [array, setArray] = useState(["0, "]);
+    var [msg, setMsg] = useState("");
+    function handleClick() {
+        var result = Math.floor(Math.random() * 40) + 1;
+        setLuckyNumber(result);
+        if (array.includes((result + ", "))) {
+            setMsg("Este número já foi sorteado!");
+        } else {
+            setMsg("");
+            setArray(0 + ", ");
+            array.push(result+ ", ");
+            setArray(array);
+        }
+        
+    }
+    
+    return <div className ={styles.container}>
 
-function handleClick() {
-    setLuckyNumber(Math.floor(Math.random() * 40)+ 1);
-}
-
-
-    return (
-        <div className={styles.container}>
-            <h1>Lucky Number 🎲</h1>
-            <h1>Lucky Number = {LuckyNumber}</h1>
-            <button className={styles.button} onClick={handleClick}>im feeling lucky</button>
-            </div>
-
-        );
+            {luckyNumber === 0 ? (
+                <h1>Lucky Number 🎲</h1>) : (
+                <h1> Lucky Number: = {luckyNumber}</h1>)}
+            
+            <button className={styles.button} onClick={handleClick}>
+                I'm Feeling Lucky Today!
+            </button>
+            <h1>{array}</h1>
+            <h1>{msg}</h1>
+        </div>
 }
